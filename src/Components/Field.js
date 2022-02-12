0d787217c8ec4e4e5f24523639747a284f11480c
Pavel <kurilo.pavel@mail.ru>
2022-02-12 18:17:22 +0300

    Completed Loading

--- src/Components/Field.js
+++ src/Components/Field.js
@@ -1,20 +1,22 @@
 import {useSelector} from "react-redux";
 import {v4 as uuidv4} from "uuid";
-import {useEffect} from "react";
+import Loading from "./Loading";
 
 const Field = () => {
-  const users = useSelector((state) => state.users.users)
+  const users = useSelector((state) => state.users.users);
+  const status = useSelector((state) => state.users.loading);
   console.log(users)
 
   if(users){console.log(users.items[0].id)}
 
-
-
-  return <>
+  return <div>
+    {status === 'loading' ? <Loading/> : null}
     {users ? users.items.map(user => {
       return (
-        <div key={uuidv4()}>{user.id}</div>)
+        <div key={uuidv4()}
+        className="inline-block border border-gray-400 w-1/5"
+        >{user.id}</div>)
     }) : null}
-  </>
+  </div>
 }
 export default Field;
--- src/Components/Seach.js
+++ src/Components/Seach.js
@@ -1,15 +1,15 @@
-import {useEffect, useState} from "react";
 import {useDispatch} from "react-redux";
 import {getUsers} from "./store/usersSlice";
 
 const Seach = () => {
   const dispatch = useDispatch();
 
-  return <>
+  return <div className="text-center relative h-20">
     <input type="text"
+           className="border border-gray-300 w-1/3 italic px-3 py-1 align-middle top-1/2 relative translate-y-[-50%]"
            onChange={(e) => {
-             dispatch(getUsers(e.target.value))
+             return e.target.value ? dispatch(getUsers(e.target.value)) : null
            }}/>
-  </>
+  </div>
 }
 export default Seach;
